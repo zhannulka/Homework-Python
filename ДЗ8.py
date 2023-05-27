fcea82c638dcde2a7b125b42a330e88a01361e1f
zhannulka <124399184+zhannulka@users.noreply.github.com>
2023-05-27 22:06:34 +0300

    Update ДЗ8.py

--- ДЗ8.py
+++ ДЗ8.py
@@ -6,7 +6,7 @@ def printinfo():
 def searchinfo():
     with open('phonebook.txt', 'r', encoding='utf-8') as file:
         book = file.read().split('\n')
-        temp = input('Введите ФИО или номер для поиска: ')
+        temp = input('ведите ФИО или номер для поиска: ')
         for i in book:
             if temp in i:
                 print(i)
@@ -15,10 +15,27 @@ def newinfo():
     with open('phonebook.txt', 'a', encoding='utf-8') as file:
         file.write(input('Введите новую строку: '+ '\n') )
 
+def deleteinfo(name):
+    persons = printinfo()
+    with open("phonebook.txt", "w", encoding="utf8" ) as file:
+        for person in persons:
+            if name != person:
+                file.write(person)
+
+def changeinfo(new_name, old_name):
+    persons = printinfo()          
+    with open("phonebook.tx", "w", encoding="utf8" ) as file:
+        for person in persons:
+            if  old_name != person:
+                file.write(person)
+            else:
+                file.write(new_name + "\n")
+
+
 
 while True:
     mode = input('Выберите режим работы справочника' + '\n'
-                  +'0-чтение файла, 1-поиск, 2-добавление в файл, 3-выход: ')
+                  +'0-чтение файла, 1-поиск, 2-добавление в файл, 3-удаление, 4-замена, 5-выход: ')
     if mode == '0':
         print(printinfo())
     elif mode == '1':
@@ -26,6 +43,14 @@ while True:
     elif mode == '2':
         newinfo()
     elif mode == '3':
+        name = input('Выберите строку для удаления: ')
+        deleteinfo(name)
+    elif mode == '4':
+        old_name = input('Введите ФИО или номер для изменения: ')
+        new_name = input('Введите новую инфо: ')
+        changeinfo(new_name,old_name)
+    elif mode == '5':
         break
     else:
         print('нет режима')
+
